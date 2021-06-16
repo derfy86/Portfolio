47f74e6f542761206a5502814d48309ff09c3c91
fred <bourigeaudfrederic@yahoo.fr>
2021-06-16 19:29:30 +0200

    repository good

--- front/src/components/Animation/index.js
+++ front/src/components/Animation/index.js
@@ -4,23 +4,6 @@ import PropTypes from 'prop-types';
 
 // == Import
 import './style.scss';
-
-// == Composant
-const Animation = (firstAnimationFinished, handelFirstAnim) => (
-  <div className="animation">
-    <div className="top-block" />
-
-    <div className="animation-text">Derfy Dev Web</div>
-
-    <div className="bottom-block" />
-  </div>
-);
-// == Import npm
-import React from 'react';
-import PropTypes from 'prop-types';
-
-// == Import
-import './style.scss';
 import hudVideo from '../../assets/hud.mp4';
 
 // == Composant
@@ -58,14 +41,6 @@ const Animation = ({ firstAnimationFinished, handelFirstAnim }) => {
     </div>
   );
 };
-
-Animation.prototypes = {
-  firstAnimationFinished: PropTypes.bool.isRequired,
-  handelFirstAnim: PropTypes.func.isRequired,
-};
-
-// == Export
-export default Animation;
 
 Animation.prototypes = {
   firstAnimationFinished: PropTypes.bool.isRequired,
